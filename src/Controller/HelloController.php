6d8c8a7c174dab4de14ea6cf1af60ef4f378d353
Duarte Cancela <duartecancela@gmail.com>
2023-09-10 09:34:43 +0100

    update route

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
         return new Response("HI!");
