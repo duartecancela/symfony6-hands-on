36b95e7175e645d36d65dc4aecfe0f3bc885e731
Duarte Cancela <duartecancela@gmail.com>
2023-10-07 21:56:01 +0100

    Voters

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -29,6 +29,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    #[IsGranted(MicroPost::VIEW, 'post')]
     public function showOne(MicroPost $post): Response
     {
        //dd($post);
@@ -66,7 +67,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
-    #[IsGranted('ROLE_EDITOR')]
+    #[IsGranted(MicroPost::EDIT, 'post')]
     public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
     {
         $form = $this->createForm(MicroPostType::class, $post);
